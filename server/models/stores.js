2657f9522b1fe0dc0a192452b660320aa8ea465e
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2018-05-18 14:29:50 -0700

    first commit

---
+++ server/models/stores.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const storeSchema = mongoose.Schema({
+  name:{
+    type:String,
+    required:true,
+    trim:true
+  },
+  address:{
+    type:String,
+    required:true,
+    trim:true
+  },
+  phone:{
+    type:Number
+  }
+})
+const Store = mongoose.model('Store',storeSchema);
+module.exports ={Store};
